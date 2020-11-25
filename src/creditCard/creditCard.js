5e4cc34a21d7ff7ba51661de08dac452fb526110
samuFerragut <sferragut@cifpfbmoll.eu>
2020-11-25 20:00:51 +0100

    refactor(creditCard): Usar this y exportar modulo

--- src/creditCard/creditCard.js
+++ src/creditCard/creditCard.js
@@ -1,32 +1,17 @@
-var owner = "";
-var number = "";
-var credit = 3000;
-var symbol = "EZI";
-
-export { CreditCart, getterCredit, getterNumber, pay};
-
-function CreditCart(propietario, numeroTarjeta) {
-    this.propietario = propietario;
-    this.numeroTarjeta = numeroTarjeta;
+function CreditCard(owner, number) {
+    this.owner = owner;
+    this.number = number;
+    this.credit = 3000;
+    this.symbol = "EZI";
 }
-  
-var getterCredit = {
-    get credit() {
-        return (this.credit = credit);
-    },
-};
 
-var getterNumber = {
-    get number() {
-        return (this.number = number);
-    },
-};
-
-function pay (pago) {
-    if (credit >= pago) {
-        credit -= pago;
+CreditCard.pay = function (pago) {
+    if (this.credit >= pago) {
+        this.credit -= pago;
         return true;
     } else {
         return false;
     }
 }
+
+module.exports = CreditCard
