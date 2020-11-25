26e36cdfa569500aa5cb24f02f06c3dfdcaf27bc
samuFerragut <sferragut@cifpfbmoll.eu>
2020-11-25 21:50:34 +0100

    feat(creditCard.test): Testear función pago

--- src/creditCard/test/creditCard.test.js
+++ src/creditCard/test/creditCard.test.js
@@ -11,3 +11,18 @@ test("Crear tarjeta", () => {
     expect(abradolph.credit).toBe(3000)
     expect(abradolph.symbol).toBe("EZI");
 })
+
+test("Pago menor 3000", () => {
+    abradolph.pay(2000);
+    expect(abradolph.credit).toBe(1000);
+})
+
+test("Pago igual 3000", () => {
+    abradolph.pay(3000);
+    expect(abradolph.credit).toBe(0);
+})
+
+test("Pago mayor 3000", () => {
+    abradolph.pay(3001);
+    expect(abradolph.credit).toBe(3000);
+})
