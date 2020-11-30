fc596b8634c1201e8b7ef4049211beec6c87261c
samuFerragut <sferragut@cifpfbmoll.eu>
2020-11-30 16:05:19 +0100

    feat(crystalExpender): Añadir dispatch crystalExpender

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //imports 
 var CreditCard =  require("./creditCard/creditCard");
 var singleUfosPark = require("./ufosPark/ufosPark");
+var CrystalExpender = require("./crystalExpender/crystalExpender")
 
 var abradolph = new CreditCard("Abradolph", "12345678");
   
@@ -10,59 +11,90 @@ var abradolph = new CreditCard("Abradolph", "12345678");
 
   // UfosPark
 
+  console.log("<------------------------------>")
   var ufosPark = singleUfosPark.getPark();
+  let ufos = ["Uber", "Cabify"];
+  for (let position in ufos) {
+    ufosPark.add(ufos[position]);
+  }
+  console.log("Lista de ovnis")
+  console.log(ufos)
+
+  console.log("<------------------------------>")
   console.log("\n" + "Ovni de Abradolph" + "\n");
-  console.log(ufosPark.getUfoOf());
+  ufosPark.dispatch(abradolph)
+  console.log("Ovni de Abradolph: " + ufosPark.getUfoOf(abradolph.number));
   console.log("Credito de Abradolph: " + abradolph.credit);
 
   console.log("\n" + "Abradolph quiere otro ovni" + "\n");
+  ufosPark.dispatch(abradolph)
   console.log("Su credito no ha cambiado: " + abradolph.credit);
   console.log("Ovni de Abradolph: " + ufosPark.getUfoOf(abradolph.number));
 
   // UfosPark GearHead
 
-  var gearHead = new CreditCard("GearHead", "12345678");
- 
+  console.log("<------------------------------>")
+  var gearHead = new CreditCard("GearHead", "12345679");
+  console.log("\n" + "Tarjeta de GearHead" + "\n");
+  gearHead.pay(3000)
+  console.log(gearHead);
+  
   console.log("Su credito es cero: " + gearHead.credit);
+  ufosPark.dispatch(gearHead)
   console.log("No puede reservar ovni: " + ufosPark.getUfoOf(gearHead.number));
 
   // UfosPark Squanchy
 
-  var squanchy = new CreditCard("Squanchy", "12345678");
-
+  console.log("<------------------------------>")
+  var squanchy = new CreditCard("Squanchy", "12345677");
+  console.log("\n" + "Tarjeta de Squanchy" + "\n");
+  console.log(squanchy);
+  ufosPark.dispatch(squanchy)
   console.log("Su credito es: " + squanchy.credit);
   console.log("Su ovni es: " + ufosPark.getUfoOf(squanchy.number));
 
   // UfosPark Morty
 
-  var morty = new CreditCard("Morty", "12345678");
+  console.log("<------------------------------>")
+  var morty = new CreditCard("Morty", "12345676");
+  console.log("\n" + "Tarjeta de Morty" + "\n");
+  console.log(morty);
 
+  ufosPark.dispatch(morty)
   console.log("Su credito no ha cambiado: " + morty.credit);
   console.log("No hay ovni Morty: " + ufosPark.getUfoOf(morty.number));
 
   // Flota de ovnis
 
+  console.log("<------------------------------>")
   console.log(ufosPark);
 
   // CrystalExpender
 
+  console.log("<------------------------------>")
+  var crystalExpender = new CrystalExpender(5, 50);
   console.log("\n" + "Packs" + "\n");
-  console.log(packExpender);
+  console.log(crystalExpender);
 
   // Expender Abradolph
 
+  console.log("<------------------------------>")
   console.log("\n" + "Abradolph compra su pack" + "\n");
-  console.log("Packs: " + packExpender);
+  crystalExpender.dispatch(abradolph)
+  console.log("Packs: " + crystalExpender.stock);
   console.log("Credito de Abradolph: " + abradolph.credit);
 
   // Expender GearHead
 
+  console.log("<------------------------------>")
   console.log("\n" + "GearHead sin credito para su pack" + "\n");
-  console.log("Packs: " + packExpender);
+  crystalExpender.dispatch(gearHead)
+  console.log("Packs: " + crystalExpender.stock);
   console.log("Credito de GearHead: " + gearHead.credit);
 
   // Receptivo
 
+  console.log("<------------------------------>")
   console.log("Llega Squanchy");
   console.log("Llega GearHead");
   console.log("Llega Birdpearson!");
@@ -70,5 +102,6 @@ var abradolph = new CreditCard("Abradolph", "12345678");
 
   // 10!!
 
+  console.log("<------------------------------>")
   console.log("\n" + "Pedidos de RickMenus: " + "\n");
   console.log("\n" + "Creditos de los invitad@s: " + "\n");
